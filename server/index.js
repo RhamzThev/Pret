71b11f4bfd8117f23b9278ba70c04b805749869c
Rhamsez Thevenin <rhamzthev@gmail.com>
2024-09-13 10:58:38 -0400

    added stubs for future releases

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const OpenAI = require('openai')
+
+// const { ElevenLabsClient } = require("elevenlabs");
+
+
+
 var cors = require('cors')
 require('dotenv').config()
 
@@ -10,39 +16,49 @@ app.use(express.json());
 
 const port = 3000
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+// const elevenlabs = new ElevenLabsClient({
+//     apiKey: process.env.ELEVENLABS_API_KEY
+// })
+
 app.post('/', async (req, res) => {
 
     const input = req.body.input
 
-    await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-            "content-type": 'application/json',
-            'Authorization': `Bearer ${process.env.API_KEY}`
-        },
-        body: JSON.stringify({
-            "model": "gpt-4o-mini",
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "Your role is to simplify text (i.e. Reiterate the text in layman's terms). Don't explain it as if you're a median trying to make sense of another person's writing. Explain it as if you're the author of the given input, and you restate what you said in simpler (i.e., layman's) terms."
-                },
-                {
-                    "role": "user",
-                    "content": input
-                }
-            ]
-        })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Completed.")
-            const content = data.choices[0].message.content;
-            res.json({ "output": content });
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    const CHARACTERS = 5000
+    const TOKENS = CHARACTERS * 0.25
+
+    const chatResponse = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: "Your role is to simplify text (i.e. Reiterate the text in layman's terms). Don't explain it as if you're a median trying to make sense of another person's writing. Explain it as if you're the author of the given input, and you restate what you said in simpler (i.e., layman's) terms."
+            },
+            {
+                role: "user",
+                content: input
+            }
+        ],
+        max_tokens: TOKENS,
+        model: "gpt-4o-mini",
+    });
+
+    const content = chatResponse.choices[0].message.content
+
+    // const voiceResponse = await elevenlabs.generate({
+    //     voice: 'Brian',
+    //     text: content,
+    //     model_id: 'eleven_turbo_v2'
+
+    // })
+
+    res.json({
+        "output": content,
+        // "audio": voiceResponse,
+    });
 })
 
 app.listen(port, () => {
